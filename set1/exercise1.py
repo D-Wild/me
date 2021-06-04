3b673256a26d486dfea9aaaea9b3430d17ba2c43
Declan Wild <declanwild@gmail.com>
2021-06-04 15:17:51 +1000

    DONE

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -4,6 +4,9 @@ TODO: write a python script that prints "Hello world!"
 
 Look on the course website for how to do this, and for how to run the tests.
 
-https://design-computing.github.io/md/week1
+https://design-computing.python ../course/week1/tests.pygithub.io/md/week1
 
 """
+
+print("Hello world!")
+
